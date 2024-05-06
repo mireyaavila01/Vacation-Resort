b2989178ddd9b4edaeb79ccff5ec2591e17af55b
Mireya Avila <mireyaavila147@yahoo.com>
2024-05-06 16:11:55 -0400

    working on the js

---
+++ scripts/overnight_stay.js
@@ -0,0 +1,26 @@
+"use strict"
+
+//inputs
+
+const inputCheckInDate = document.getElementById("inputCheckInDate");
+const inputNumberOfNights = document.getElementById("inputNumberOfNights");
+const inputRoomQueen = document.getElementById("inputRoomQueen");
+const inputRoomKing = document.getElementById("inputRoomKing");
+const inputRoomBedroomSuite = document.getElementById("inputRoomBedroomSuite");
+const inputNumberofAdults = document.getElementById("inputNumberOfAdults");
+const inputNumberOfChildren = document.getElementById("inputNumberOfChildren");
+const inputNoDiscount = document.getElementById("inputNoDiscount");
+const inputAAA_SenDiscount = document.getElementById("inputAAA_SenDiscount");
+const inputMilitaryDiscount = document.getElementById("inputMilitaryDiscount");
+
+//button
+
+const estimateCostBtn = document.getElementById("estimateCostBtn");
+
+//output
+
+const outputOriginalRoomCost = document.getElementById("outputOriginalRoomCost");
+const outputDiscount = document.getElementById("outputDiscount");
+const outputDiscountedRoomCost = document.getElementById("outputDiscountedRoomCost");
+const outputTax = document.getElementById("outputTax");
+const outputTotalCostStay = document.getElementById("outputTotalCostStay");
